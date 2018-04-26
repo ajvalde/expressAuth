3efe22865ede4302caf5d35e2a1d6f27a440d6b6
Anthony <anthonyguitarman13@gmail.com>
2018-04-26 12:49:04 -0600

    can post to db

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Registration' });
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Registration' });
 });
 
 router.post('/register', function(req, res, next) {
-  res.render('index', { title: 'Registration Complete' });
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log(req.body.username)
+  console.log(req.body.email)
+  console.log(req.body.password)
+
+  const db = require('../db.js')
+
+  db.query('INSERT INTO users (username, email, password) VALUES(?, ?, ?)',[username, email, password], function(error, results, fields){
+    if (error) throw error;
+
+    res.render('register', { title: 'Registration Complete' });
+  })
+
 });
 
 module.exports = router;
