6e38c0139de329cb303031b8aeb646bb684a893a
Anthony <anthonyguitarman13@gmail.com>
2018-04-26 17:57:13 -0600

    validation working

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.post('/register', function(req, res, next) {
   req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
   req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
   req.checkBody('password', 'Password must be between 8-100 characters long.').len(8, 100);
-  req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.\d)(?=.[a-z])(?=.[A-Z])(?!. )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
+  req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!. )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
   req.checkBody('passwordMatch', 'Password must be between 8-100 characters long.').len(8, 100);
   req.checkBody('passwordMatch', 'Passwords do not match, please try again.').equals(req.body.password);
 
@@ -25,10 +25,10 @@ router.post('/register', function(req, res, next) {
   if (errors) {
     console.log(`errors: ${JSON.stringify(errors)}`);
 
-    res.render('register', {title: 'Registration Error '})
-  }
-
-  const username = req.body.username;
+    res.render('register', {title: 'Registration Error ', errors: errors})
+      
+  } else {
+    const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;
   
@@ -40,6 +40,9 @@ router.post('/register', function(req, res, next) {
 
     res.render('register', { title: 'Registration Complete' });
   })
+  }
+
+  
 
 });
 
